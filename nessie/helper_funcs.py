6119a068b84316de726661f35c7cb512b88ea660
Adrien Aufan Stoffels d'Hautefort <aa2439@sussex.ac.uk>
2025-08-06 14:14:58 +0100

    Update helper_funcs.py to add a b0, r0 optimization function

--- nessie/helper_funcs.py
+++ nessie/helper_funcs.py
@@ -9,6 +9,7 @@ import numpy as np
 from statsmodels.nonparametric.kde import KDEUnivariate
 from scipy.integrate import quad
 from scipy.interpolate import interp1d
+from scipy.optimize import fmin
 
 from nessie_py import calculate_s_score
 from .cosmology import FlatCosmology
@@ -178,3 +179,53 @@ def validate(value: np.ndarray[float] | float, valid_type: ValidationType) -> No
             raise ValueError(
                 f"Unknown property type: {valid_type.value}. Likely Enum needs updating."
             )
+
+def optimize_grouping_params(
+    z_cat, 
+    b0_guess=0.05, 
+    r0_guess=30, 
+    max_stellar_mass=1e15, 
+    min_group_size=2
+):
+    """
+    Optimizes the b0 and r0 parameters using a Nelder-Mead simplex optimization approach to maximize the s_tot output for a given RedshiftCatalog object.
+
+    Parameters
+    ----------
+    z_cat : RedshiftCatalog
+        RedshiftCatalog object for which to optimize the grouping parameters.
+    b0_guess : float
+        Initial guess for the b0 parameter.
+    r0_guess : float
+        Initial guess for the r0 parameter.
+    max_stellar_mass : float
+        Maximum galactic stellar mass present in the data.
+    min_group_size : int
+        Minimum size of the assigned and mock groups compared when calculating s_total.
+
+    Returns
+    -------
+    b_opt : float
+        Optimized b0 parameter.
+    r_opt : float
+        Optimized r0 parameter.
+    """
+
+    if z_cat.mock_group_ids is None:
+        raise InterruptedError(
+            "No mock group ids found. Be sure to set the mock groups ids."
+        )
+
+    def _objective(params):
+        b0, r0 = params
+        return -_calc_s_tot(z_cat, b0, r0)
+    
+    def _calc_s_tot(z_cat, b0, r0):
+        z_cat.run_fof(b0=b0, r0=r0, max_stellar_mass=max_stellar_mass)
+        s_tot = z_cat.compare_to_mock(min_group_size=min_group_size)
+        return s_tot
+    
+    res = fmin(_objective, (b0_guess, r0_guess), xtol=0.1, ftol=0.1, maxiter=50, full_output=True, dips=False)
+    b_opt, r_opt = res[0]
+
+    return b_opt, r_opt
